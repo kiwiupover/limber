1d85117ec871f87ef3b23a0201d00d7356f66ddf
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-29 20:12:58 -0400

    Today, opened 3 issues, but I think there is a path forward, so that's exciting!

--- frontend/app/helpers/state.ts
+++ frontend/app/helpers/state.ts
@@ -1,12 +1,33 @@
 import { tracked } from '@glimmer/tracking';
 
-class State {
+import { Resource } from 'ember-resources/core';
+
+import type { HelperLike } from '@glint/template';
+
+/**
+ * Every custom-manager using object needs to have two types.
+ * - one for templates / glint
+ * - one for JS/TS
+ *
+ *
+ * Because Glint doesn't have an integration with the managers,
+ * this complexity is pushed into user space.
+ *
+ * See issue report:
+ *   https://github.com/emberjs/rfcs/issues/822#issuecomment-1140541910
+ */
+class State extends Resource {
   @tracked value: unknown;
 
   update = (nextValue: unknown) => (this.value = nextValue);
   toggle = () => (this.value = !this.value);
 }
 
-export default function state() {
-  return new State();
-}
+/**
+ * Glint does not tie into any of the managers.
+ * See issue report:
+ *   https://github.com/emberjs/rfcs/issues/822
+ */
+export default State as unknown as HelperLike<{
+  Return: State;
+}>;
---
+++ frontend/types/limber/glint-registry.d.ts
@@ -0,0 +1,13 @@
+import "@glint/environment-ember-loose";
+import "@glint/environment-ember-loose/native-integration";
+import "ember-page-title/glint";
+
+// import type { ComponentLike, HelperLike, ModifierLike } from "@glint/template";
+// import type State from "limber/helpers/state";
+
+declare module "@glint/environment-ember-loose/registry" {
+  export default interface Registry {
+    // state: HelperLike<{ Args: {}, Return: State }>;
+    // attachShadow: ModifierLike<{ Args: { Positional: [State['update']]}}>;
+  }
+}
